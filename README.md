68081a5801b91614e24f3d36efaff4d560f26fb0
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2017-12-04 16:59:13 -0800

    node

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 04-12-17-Node-
+link-> https://repl.it/@Ifedy/04-12-17-Node-Task-D
---
+++ task.js
@@ -0,0 +1,25 @@
+//var input = "make money every day";
+//var arr = input.split(" ");
+//var len = arr.length;
+//console.log(len);
+
+
+
+var a = "HARRY";
+var b = "SALLY";
+
+
+if (a.length == b.length){
+    for( i = 1; i <= a.length; i++){
+        for(j = 1; j <= b.length; j++){
+            var arr;
+            if(a[i] == b[j]){
+                arr = (a[i]);
+
+                console.log(arr);
+            }
+        }
+    }
+}else{
+    console.log('They are not of equal lenght');
+}
